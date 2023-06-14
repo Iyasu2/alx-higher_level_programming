108319dc47e1e38d68e039c77a4047c1e0022b48
iyasu2 <iyasuforreal@gmail.com>
2023-06-14 20:52:21 +0300

    weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    numerator = 0
+    denominator = 0
+    for score, weight in my_list:
+        numerator += score * weight
+        denominator += weight
+    return numerator / denominator
