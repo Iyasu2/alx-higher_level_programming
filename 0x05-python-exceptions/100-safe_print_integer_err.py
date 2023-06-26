b68fe729a96b77844ab5dac8afa457ca4ece440e
iyasu2 <iyasuforreal@gmail.com>
2023-06-26 17:21:08 +0300

    safe function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,4 +8,4 @@ def safe_print_integer_err(value):
     except TypeError:
         import sys
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return False
+    return False
