e32c6bb2ac825c317f568cf040a2c25b381f9a6f
iyasu2 <iyasuforreal@gmail.com>
2023-06-27 19:44:54 +0300

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    This module contains the class square
+"""
+
+
+class Square:
+    """
+        This is a class that takes as attribute the size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
