59eec93050d7bf68ca961f22f3f0e2b2842363ce
iyasu2 <iyasuforreal@gmail.com>
2023-06-27 20:21:22 +0300

    square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+    This module contains the class square
+"""
+
+
+class Square:
+    """
+        This is a class that takes as attribute the size of the square.
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    """
+        This is a property getter function
+    """
+    @property
+    def size(self):
+        return self.__size
+
+    """
+        This is a property setter function
+    """
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = value
+
+    """
+        This is a method that returns the square of the attribute size.
+    """
+    def area(self):
+        return self.__size ** 2
