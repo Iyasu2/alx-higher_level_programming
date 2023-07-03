8a91acdc130dae8da89b6775245bc17a97839e9f
Iyasu2 <iyasuforreal@gmail.com>
2023-07-03 09:58:46 +0300

    doc

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+'''
+    This is a module which defines a class
+    The class is called Rectangle
+    it is empty
+'''
+
 
 class Rectangle:
+    '''
+        this is an empty class
+    '''
     pass
