cda9759d1118c72e8357fe7d10330fb5113e2a6b
iyasu2 <iyasuforreal@gmail.com>
2023-07-10 20:10:28 +0300

    s

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+this module contains a
+class that inherits from
+rectangle
+'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        '''
+        this is instantiation method
+        '''
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''
+        returns the area of the square
+        '''
+        return self.__size * self. __size
+
+    def __str__(self):
+        '''
+        Return a string representation of the square
+        '''
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
