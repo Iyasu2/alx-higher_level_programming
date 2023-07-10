78967bfc07eeecb3e241943455b65b1774d81a68
iyasu2 <iyasuforreal@gmail.com>
2023-07-10 19:09:25 +0300

    base gemotry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+this module contains
+a class with two public
+instance methods
+'''
+
+
+class BaseGeometry:
+    '''
+    this class has 2 methods
+    '''
+    def area(self):
+        '''
+        this method raises an exception
+        '''
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
