0f40b9ccc181a69a497a171b9dade066171f3041
iyasu2 <iyasuforreal@gmail.com>
2023-07-11 20:31:13 +0300

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+this module contains a class Student
+with 3 public instance attributes
+and a public instance method
+'''
+
+
+class Student:
+    '''
+    this is the class Student
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''
+        this is the instantiation method
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        this is the public instance method
+        that returns the dictionary
+        '''
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
