28bcc660e8b3761ba2dca123ac2a6a046db57348
iyasu2 <iyasuforreal@gmail.com>
2023-07-11 20:16:53 +0300

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+this module contains a function
+class_to_json that returns a dictionary
+of all the object's attributes
+'''
+
+
+def class_to_json(obj):
+    '''
+    this function returns a dictionary of 
+    obj's attributes
+    '''
+    return obj.__dict__
