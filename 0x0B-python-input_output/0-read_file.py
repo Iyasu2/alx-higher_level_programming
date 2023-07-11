386eb28fa9c37af24c1ee170d73eaed4a4ef19fb
Iyasu2 <iyasuforreal@gmail.com>
2023-07-11 08:44:10 +0300

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+this module contains a function
+read_file that reads a file
+and prints it out
+'''
+def read_file(filename=""):
+    '''
+    reads filename and prints it out
+    '''
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
