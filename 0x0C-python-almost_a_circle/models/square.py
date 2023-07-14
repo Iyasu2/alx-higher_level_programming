5f0f9540f6a3c46c350d0116fb7fef38001359c9
Iyasu2 <iyasuforreal@gmail.com>
2023-07-14 09:42:41 +0300

    square 2

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,26 @@ class Square(Rectangle):
         '''
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        '''
+        getter for the size attribute
+        '''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''
+        setter for the size attribute
+        '''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
+
     def __str__(self):
         '''
         this method overrides the str from the parent class
