ace405896dd84fda03e8a074be1f7edd9ce54e6b
Iyasu2 <iyasuforreal@gmail.com>
2023-07-14 14:59:01 +0300

    ss

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ this module contains the class
 Base that manages id attributes
 of other classes
 '''
+import json
 
 
 class Base:
@@ -21,3 +22,22 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''
+        changes a dict to a json representation
+        '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        with open(filename, mode="w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_dicts))
