48b36b1429f04d6cef01ecdf093f229cad9a2194
iyasu2 <iyasuforreal@gmail.com>
2023-09-16 19:38:58 +0300

    talking

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+this module contains code that
+lists all states from a
+database
+'''
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
+                            sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).first()
+    if states is None:
+        print("Nothing")
+    else:
+        print(f'{states.id}: {states.name}')
+
+    session.close()
