b4c658ea0b2fa44d01c398786ac35e4460c7e807
iyasu2 <iyasuforreal@gmail.com>
2023-09-30 17:29:49 +0300

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+this module contains code
+that fetches a status code
+from a url
+'''
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+
+    print("Body response:")
+    print("\t - type:", type(html))
+    print("\t - content:", html)
+    print("\t - utf8 content:", html.decode('utf-8'))
