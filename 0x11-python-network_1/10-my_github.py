7054dfc1fda93e0ebd36e3129f1d28538231ec9b
iyasu2 <iyasuforreal@gmail.com>
2023-10-01 11:03:47 +0300

    github_id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+this module uses requests
+"""
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+
+    response = requests.get(url, auth=HTTPBasicAuth(username, passwd))
+    json_obj = response.json()
+    print(json_obj.get("id"))
