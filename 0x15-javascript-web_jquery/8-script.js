00bf5b750dcebca7fe303488a8d4bc98a376d62d
iyasu2 <iyasuforreal@gmail.com>
2023-11-01 18:51:58 +0300

    s

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$.get("https://swapi-api.alx-tools.com/api/films/?format=json", function (data) {
+	  const movies = data.results;
+  
+	  movies.forEach(function (movie) {
+		$("#list_movies").append($("<li>").text(movie.title));
+	  });
+	});
+  });
